c660d74d81104bdf1961adb48c7936962e0b4284
n0t3-droid <n0t3-droid@users.noreply.github.com>
2026-08-25 00:20:49 +0200

    chore: remove generated local state from version control

--- fix_role.py
+++
@@ -1,9 +0,0 @@
-with open('src/bots.rs', 'r') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if "let role_color" in line and "bot.companion" in line:
-        lines[i] = "    let role_color = bot_role_color(bot.role);\n"
-
-with open('src/bots.rs', 'w') as f:
-    f.writelines(lines)
